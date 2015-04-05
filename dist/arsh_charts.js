2829cf3948c33e8bc14351f8f1975138fcf84478
AmirAli Akbari <akbari@arsh.co>
2015-04-06 00:41:13 +0430

    version 0.0.2, including dist files

---
+++ dist/arsh_charts.js
@@ -0,0 +1,8 @@
+/** Arsh Chart JS Helper Library
+
+    Mainly acting as a bower meta-package for arsh-charts python library
+     to easily include needed javascript dependencies.
+
+    Released by Arsh (arsh.co) under GPL v2 (or any later version).
+    @preserve
+**/
